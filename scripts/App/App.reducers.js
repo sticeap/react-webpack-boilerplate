ea4050ea929e4b86c61e1277ffde9622f6e308c4
Sticea Paul <paul.sticea@thinslices.com>
2016-12-07 21:14:03 +0200

    aa

--- scripts/App/App.reducers.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-
-import sessionReducer from 'commons/Session/Session.reducer'
-import aboutReducer from 'About/About.reducer'
-
-export default combineReducers({
-    session: sessionReducer,
-    about: aboutReducer
-})
--- scripts/commons/lib/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    apiURL: 'http://jsonplaceholder.typicode.com'
-}
-
-export default config
